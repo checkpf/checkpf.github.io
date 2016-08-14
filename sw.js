2071ae935a8355f1010fca37e9f2f12cbd4ed730
abs24 <abs24@users.noreply.github.com>
2016-08-14 17:36:04 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 
 importScripts('serviceworker-cache-polyfill.js');
 
-var CACHE_NAME = 'my-site-cache-v2';
+var CACHE_NAME = 'my-site-cache-v3';
 var urlsToCache = [
   '/angular.min.js',
   '/angular-aria.min.js',
@@ -27,7 +27,7 @@ self.addEventListener('install', function(event) {
         return cache.addAll(urlsToCache);
       })
   );
-  var cacheWhitelist = ['my-site-cache-v2'];
+  var cacheWhitelist = ['my-site-cache-v3'];
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
